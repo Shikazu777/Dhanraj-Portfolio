effaeb72c2ae67a08ab58a950b2a75a36b720e23
Shikazu777 <dhanrax@gmail.com>
2025-11-30 19:03:52 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-/** @type {import('next').NextConfig} */
+const repoName = "Dhanraj-Portfolio";
+
 const nextConfig = {
   output: "export",
+  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
   images: {
     unoptimized: true,
   },
